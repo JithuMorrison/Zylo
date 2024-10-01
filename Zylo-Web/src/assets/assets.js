b2145ab43687791c692220aa2f137f33df0f9da7
JithuMorrison <jithus2004@gmail.com>
2024-10-01 19:36:08 +0530

    modified files

---
+++ Zylo-Web/src/assets/assets.js
@@ -0,0 +1,34 @@
+import bell from './bell.png'
+import img10 from './karoke.jpeg'
+import img1 from './pop.jpg'
+import home from './house.jpg'
+import arrow from './arrow.png'
+import stack from './stack1.png'
+import search from './search.jpg'
+
+export const assets = {
+    bell,
+    home,
+    arrow,
+    stack,
+    search
+}
+export const albumdata = [
+    {
+        id:1,
+        name: "Trending Data",
+        image: img10,
+        desc: "Your Weekly update",
+        bgColor: "#742a2a",
+    }
+]
+export const songsdata = [
+    {
+        id:0,
+        name: "Song Two",
+        image: img1,
+        file: img1,
+        desc: "put a smile",
+        duration: "3:00",
+    }
+]
