ff982b8696b22c402e9ba066d1354ec9891c52d8
JithuMorrison <jithus2004@gmail.com>
2024-10-01 20:49:12 +0530

    Added display.jsx

--- Zylo-Web/src/App.jsx
+++ Zylo-Web/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SideBar from './component/SideBar'
 import Player from './component/Player'
+import Display from './component/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <SideBar/>
+        <Display/>
       </div>
       <Player/>
     </div>
